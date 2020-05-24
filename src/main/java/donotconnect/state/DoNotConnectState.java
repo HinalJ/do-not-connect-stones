31f788ea13838364d7a03e8ae7cf720add0d5891
Hinal Jogani <hinaljogani02@gmail.com>
2020-05-24 18:58:35 +0200

    Initial Files

---
+++ src/main/java/donotconnect/state/DoNotConnectState.java
@@ -0,0 +1,76 @@
+package donotconnect.state;
+
+public class DoNotConnectState {
+    private char[][] board = new char[5][5];
+
+    public boolean isFinished(int row, int col)
+    {
+        //Horizontal
+        if(row-2>=0)
+            if((board[row-2][col]==board[row-1][col]) && (board[row-1][col]==board[row][col]))
+                return true;
+        if(row+2<=4)
+            if((board[row][col]==board[row+1][col]) && (board[row+1][col]==board[row+2][col]))
+                return true;
+        if((row-1>=0) && (row+1<=4))
+            if((board[row-1][col]==board[row][col]) && board[row][col]==board[row+1][col])
+                return true;
+
+        //Vertical
+        if(col-2>=0)
+            if((board[row][col-2]==board[row][col-1]) && (board[row][col-1]==board[row][col]))
+                return true;
+        if(col+2<=4)
+            if((board[row][col]==board[row][col+1]) && (board[row][col+1]==board[row][col+2]))
+                return true;
+        if((col-1>=0) && (col+1<=4))
+            if((board[row][col-1]==board[row][col]) && board[row][col]==board[row][col+1])
+                return true;
+
+        //Diagonal
+        if(row-2>=0 && col-2>=0)
+            if(board[row-2][col-2]==board[row-1][col-1] && board[row-1][col-1]==board[row][col])
+                return true;
+        if(row+2<=4 && col+2<=4)
+            if(board[row+2][col+2]==board[row+1][col+1] && board[row+1][col+1]==board[row][col])
+                return true;
+        if((row-1>=0 && row+1<=4) && (col-1>=0 && col+1<=4))
+            if(board[row-1][col-1]==board[row][col] && board[row][col]==board[row+1][col+1])
+                return true;
+
+        //Anti-Diagonal
+        if(row-2>=0 && col+2<=4)
+            if(board[row-2][col+2]==board[row-1][col+1] && board[row-1][col+1]==board[row][col])
+                return true;
+        if(row+2<=4 && col-2>=0)
+            if(board[row+2][col-2]==board[row+1][col-1] && board[row+1][col-1]==board[row][col])
+                return true;
+        if((row+1<=4 && col-1>=0) && (col+1<=4 && row-1>=0))
+            if(board[row+1][col-1]==board[row][col] && board[row][col]==board[row-1][col+1])
+                return true;
+
+        return false;
+    }
+
+
+    public boolean isValidMove(int row, int col)
+    {
+        return board[row][col]!='o' && board[row][col]!='x';
+    }
+
+    public char draw(int row, int col, char turn)
+    {
+        board[row][col] = turn;
+        return turn;
+    }
+
+    public boolean isAllFilled(){
+        for(int i = 0; i<5; i++)
+            for(int j = 0; j<5; j++)
+                if (board[i][j] != 'x' && board[i][j] != 'o')
+                    return false;
+
+        return true;
+    }
+
+}
